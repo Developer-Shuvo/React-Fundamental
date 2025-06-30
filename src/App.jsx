237bae940e6e7f121d1e4e73c6c6d6d487cb4f00
SHUVO <im.computer.engineeer@gamil.com>
2025-06-30 14:15:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Name></Name>
+    </>
+  );
+}
+
+function Name() {
+  const name = "Shuvo";
+  const age = 22;
+  const profession = "Web Developer";
+  return (
+    <div>      
+    <h1 className="title-style"> Information Desk</h1>
+    <h2>Name is: {name}</h2>
+    <h2>Age is: {age}</h2>
+    <h2>Profession is: {profession}</h2>
+  </div>
+     
+  );
+}
+
+
+
+export default App;
